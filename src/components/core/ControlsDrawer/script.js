9b56ef8701d71f04becbd521461f023048b133e5
Caur <caur144@gmail.com>
2024-04-08 14:08:16 +0100

    Update script.js

--- src/components/core/ControlsDrawer/script.js
+++ src/components/core/ControlsDrawer/script.js
@@ -7,9 +7,7 @@ import GlobalSettings from 'paraview-glance/src/components/core/GlobalSettings';
 export default {
   name: 'ControlsDrawer',
   components: {
-    Datasets,
-    EditTools,
-    GlobalSettings,
+    
   },
   data() {
     return {
